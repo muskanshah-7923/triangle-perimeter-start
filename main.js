11cba93e22b51abd14900424b085080fe434d26f
muskanshah-7923 <124912381+muskanshah-7923@users.noreply.github.com>
2024-03-04 10:03:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   // Get Input Variables
-  let xAin = +document.getElementById("x1").value;
-  let xBin = +document.getElementById("y2").value;
-  let yAin = +document.getElementById("x2").value;
-  let yBin = +dpcument.getElementById("y2").value;
-  let xCin = +document.getElementById("x3").value;
-  let yCin = +document.getElementById("y3").value;
+  let xA = document.getElementById("x1").value;
+  let xB = document.getElementById("y2").value;
+  let yA = document.getElementById("x2").value;
+  let yB = dpcument.getElementById("y2").value;
+  let xC = document.getElementById("x3").value;
+  let yC = document.getElementById("y3").value;
 
   // Process
-  let AB = dist(xAin, xBin, yAin, yBin);
-  let AC = dist(xAin, yAin, xCin, yCin);
-  let BC = dist(xBin, yBin, xCin, yCin);
+  let AB = dist(xA, xB, yA, yB);
+  let AC = dist(xA, yA, xC, yC);
+  let BC = dist(xB, yBin, xC, yC);
   let perimeter = AB + AC + BC;
 
   
@@ -22,9 +22,9 @@ function btnClicked() {
   document.getElementById("output").innerHTML = perimeter;
 }
 
-function dist(x1, y1, x2, y2) {
-  let rise = y2 - y1;
-  let run = x2 - x1;
-  let d = Math.sqrt(run ** 2 + rise ** 2);
-  return d;
-}
+// function dist(x1, y1, x2, y2) {
+//   let rise = y2 - y1;
+//   let run = x2 - x1;
+//   let d = Math.sqrt(run ** 2 + rise ** 2);
+//   return d;
+// }
