824fa0008a5986141123786cdc6251532b9b58bf
muskanshah-7923 <124912381+muskanshah-7923@users.noreply.github.com>
2024-03-08 18:17:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -25,14 +25,30 @@
 //   document.getElementById("output").innerHTML = perimeter;
 
 // Triangle Perimeter
-
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  let xA = document.getElementById("x1");
-  let xB = document.getElementById("x2");
-  let yA = document.getElementById("y1");
-  let yB = document.getElementById("y2");
+  // Get vertex coordinates
+  let xA = +(document.getElementById("x1").value);
+  let yA = +(document.getElementById("y1").value);
+  let xB = +(document.getElementById("x2").value);
+  let yB = +(document.getElementById("y2").value);
+  let xC = +(document.getElementById("x3").value);
+  let yC = +(document.getElementById("y3").value);
+
+  // Calculate distances
+  let AB = dist(xA, xB, yA, yB);
+  let AC = dist(xA, xC, yA, yC);
+  let BC = dist(xB, xC, yB, yC);
+
+  // Display distances8
+  document.getElementById("output1").innerHTML = AB.toFixed(2);
+  document.getElementById("output2").innerHTML = AC.toFixed(2);
+  document.getElementById("output3").innerHTML = BC.toFixed(2);
+
+  // Calculate and display perimeter
+  let perimeter = AB + AC + BC;
+  document.getElementById("output").innerHTML = perimeter.toFixed(2);
 }
 
 function dist(x1, x2, y1, y2) {
